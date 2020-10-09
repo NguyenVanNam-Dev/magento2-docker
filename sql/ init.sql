ad8442f91357f9cd5c3173861c17a2744a3f21c9
Nam Nguyen <mrnamnguyen18@gmail.com>
2020-10-09 07:05:07 +0700

    docker update install magento

--- sql/ init.sql
+++ sql/ init.sql
@@ -4,10 +4,3 @@ CREATE TABLE IF NOT EXISTS `user` (
     `password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB;
-
-CREATE TABLE IF NOT EXISTS `userssss` (
-                                      `id` INT unsigned NOT NULL AUTO_INCREMENT,
-                                      `email` VARCHAR(50) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                                      `password` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-                                      PRIMARY KEY (`id`)
-) ENGINE=InnoDB;
